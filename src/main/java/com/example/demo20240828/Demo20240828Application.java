2bc70760d938a639321edda883348c3392ea2e1f
Shinya-Takiyoshi <garo.savior@gmail.com>
2024-08-28 17:23:32 +0900

    first commit

---
+++ src/main/java/com/example/demo20240828/Demo20240828Application.java
@@ -0,0 +1,13 @@
+package com.example.demo20240828;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo20240828Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo20240828Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo20240828/Demo20240828ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo20240828;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo20240828ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
